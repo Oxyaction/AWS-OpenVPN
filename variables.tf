677aab3adb77ed942afd0465946a3d02463fbc6d
Oxyaction <alexkomp.ua@gmail.com>
2018-07-14 01:41:37 +0300

    initial

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "key_name" {
+  default = "default"
+}
+
+variable "public_key_path" {}
+
+variable "region" {
+  default = "us-east-1"
+}
